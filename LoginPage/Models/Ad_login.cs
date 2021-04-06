e63a99842f0bc6fefaf52bafce705a55b44b2d86
Chaitu0599 <chaitus.munipalle@gmail.com>
2021-04-06 19:48:48 +0530

    initial commit

---
+++ LoginPage/Models/Ad_login.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LoginPage.Models
+{
+    public class Ad_login
+    {
+        public string Username { get; set; }
+        public string Upassword { get; set; }
+
+    }
+}
---
+++ LoginPage/Models/db.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Data;
+using System.Data.SqlClient;
+using System.Configuration;
+using LoginPage.Models;
+namespace LoginPage.Models
+{
+    public class db
+    {
+        SqlConnection con = new SqlConnection("Data Source=localhost\\SQLEXPRESS;Initial Catalog=Login;Integrated Security=True");
+        
+        public int LoginCheck(Ad_login ad)
+        {
+            SqlCommand com = new SqlCommand("SP_login", con);
+            com.CommandType = CommandType.StoredProcedure;
+            com.Parameters.AddWithValue("@Username", ad.Username);
+            com.Parameters.AddWithValue("@Upassword", ad.Upassword);
+            SqlParameter oblogin = new SqlParameter();
+            oblogin.ParameterName = "@Isvalid";
+            oblogin.SqlDbType = SqlDbType.Bit;
+            oblogin.Direction = ParameterDirection.Output;
+            com.Parameters.Add(oblogin);
+            con.Open();
+            com.ExecuteNonQuery();
+            int res = Convert.ToInt32(oblogin.Value);
+            con.Close();
+            return res;
+        }
+    }
+}
